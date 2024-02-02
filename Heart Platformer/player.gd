45293cea60ba637e9b75173f0d9f228ca523f33e
eronmoran <121971966+eronmoran@users.noreply.github.com>
2024-02-02 14:32:12 -0800

    37:47

--- Heart Platformer/player.gd
+++ Heart Platformer/player.gd
@@ -10,21 +10,15 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 func _physics_process(delta):
 	apply_gravity(delta)
 	handle_jump()
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction != 0:
-		velocity.x = move_toward(velocity.x, SPEED * direction, ACCELERATION * delta)
-	else:
-		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
-
+	var input_axis = Input.get_axis("ui_left", "ui_right")
+	handle_acceleration(input_axis, delta)
+	apply_fricton(input_axis, delta)
 	move_and_slide()
-
+	
 func apply_gravity(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		
+
 func handle_jump():
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_accept"):
@@ -32,3 +26,7 @@ func handle_jump():
 		else: 
 			if Input.is_action_just_released("ui_accept") and velocity.y < JUMP_VELOCITY / 2:
 				velocity.y = JUMP_VELOCITY / 2
+				
+func handle_acceleration(input_axis, delta):
+	if input_axis == 0:
+		velocity.x = move_toward()
